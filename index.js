56a7b12d7da6d48445e4162f6ebde7e94f255069
robbiereta <robbie.reta67@gmail.com>
2023-05-18 17:46:14 +0000

    .

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var mailerRouter = require("./routes/mailer");
+
+app.get('/mailer',mailerRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
